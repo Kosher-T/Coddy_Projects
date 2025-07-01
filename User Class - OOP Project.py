f1568a4f5d963cb3299e1d5b1d41611e7c2abbf1
Itoro Usa <itorousa@gmail.com>
2025-07-01 21:07:30 +0100

    Fix formatting and ensure newline at end of file in User class methods

--- User Class - OOP Project.py
+++ User Class - OOP Project.py
@@ -9,3 +9,6 @@ class User:
 
     def setLocation(self, location):
         self._location = location
+    
+    def info(self):
+        print(f"Username: {self.username}\nEmail: {self.email}\nLocation: {self._location}")
