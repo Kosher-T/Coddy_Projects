1ef77c015cf5f02e90f0669c7a62e4b8e568ace2
Itoro Usa <itorousa@gmail.com>
2025-07-01 20:59:53 +0100

    Refactor User class to include location management methods

--- User Class - OOP Project.py
+++ User Class - OOP Project.py
@@ -2,3 +2,10 @@ class User:
     def __init__(self, username, email):
         self.username = username
         self.email = email
+        self._location = "unset"
+    
+    def getLocation(self):
+        return self._location
+
+    def setLocation(self, location):
+        self._location = location
