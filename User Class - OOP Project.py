cc9e449b4daf3f18cc63dd3de2de5d56c96de2f6
Itoro Usa <itorousa@gmail.com>
2025-07-04 22:42:13 +0100

    Add methods clearSkill and calculateSalary. Too lazy to explain how they work

--- User Class - OOP Project.py
+++ User Class - OOP Project.py
@@ -23,3 +23,13 @@ class User:
     def showSkills(self):
         for key, val in sorted(self.skills.items()):
             print(f"{key}: {val}")
+
+    def clearSkill(self, skill):
+        if skill in self.skills:
+            del(self.skills[skill])
+
+    def calculateSalary(self):
+        sum_levels = 0
+        for key, value in self.skills.items():
+            sum_levels += value
+        return sum_levels * 100
